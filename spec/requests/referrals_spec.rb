f97acbed7a5f0d5c1857fbd2cc75f57472f9544f
Syed Aslam <aslam.maqsood@gmail.com>
2024-12-22 21:02:46 +0530

    assert and preview email

--- spec/requests/referrals_spec.rb
+++ spec/requests/referrals_spec.rb
@@ -15,6 +15,15 @@ RSpec.describe 'Referrals', type: :request do
 
       expect(response).to have_http_status(:ok)
       expect(response.body).to include('Referral sent successfully')
+
+      # Check that one email was sent
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+
+      # Inspect the email
+      email = ActionMailer::Base.deliveries.last
+      expect(email.to).to include('friend@example.com')
+      expect(email.subject).to eq('You are invited to join DirectShifts')
+      expect(email.body.encoded).to include('Join Now')
     end
   end
 end
